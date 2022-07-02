9b1074195b0d764155765e54ed86f41943a66bfc
Will McElhenney <sir.sleepy93@gmail.com>
2022-07-01 23:26:31 -0500

    Need a session, this version of GET works

--- IssuetrakAPI/IssuetrakAPI.py
+++ IssuetrakAPI/IssuetrakAPI.py
@@ -1,5 +1,6 @@
 import base64
 import hmac
+import http.client
 import re
 from datetime import datetime
 from hashlib import sha512
@@ -10,18 +11,16 @@ import pdb
 import hyperlink
 import requests
 
-
 RE_CLEAN_HYPERLINK = re.compile(r'(?<!^https:)/{2,}')
-
 
 class IssuetrakAPI:
 	def __init__(self, api_key: str, api_url: str):
 		self.api_key = api_key
 		self.api_url = api_url
+		self.session = requests.Session()
 
 
 	def __compute_hash(self, message:str) -> str:
-		hash_text = None
 		api_key_bytes = bytes(self.api_key, 'UTF-8')
 		message_bytes = bytes(message, 'UTF-8')
 
@@ -36,13 +35,13 @@ class IssuetrakAPI:
 		"""
 		full_url = '/'.join([self.api_url, endpoint_url])
 		
-
 		# Clean extraneous slashes
 		full_url = RE_CLEAN_HYPERLINK.sub('/', full_url)
 
-		# Get hyperlink and normalize
+		# Make hyperlink object and normalize
 		url = hyperlink.parse(full_url)
 		url = url.normalize()
+		url = url.replace(port=443)
 		return url
 	
 
@@ -52,13 +51,13 @@ class IssuetrakAPI:
 		"""
 		request_id = str(uuid4()).lower()
 		timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f0Z")
-		url_path = '/'.join(url.path)
-		url_path = '/' + url_path.lower()
-		print(url_path)
+		absolute_path = '/'.join(url.path)
+		absolute_path = '/' + absolute_path.lower()
 		
-		message = '\n'.join([http_verb, request_id, timestamp, url_path, request_query, request_body])
+		message = '\n'.join([http_verb, request_id, timestamp, absolute_path, request_query, request_body])
 		hashed_message = self.__compute_hash(message)
-		
+		print(f'Message: {message}')
+		print(f'Hash: {hashed_message}')
 		headers = {"X-IssueTrak-API-Request-ID": request_id,
 			"X-IssueTrak-API-Timestamp": timestamp,
 			"X-IssueTrak-API-Authorization": hashed_message,
@@ -66,11 +65,17 @@ class IssuetrakAPI:
 
 		return headers
 
+	def __api_call(self, http_verb, endpoint_url, request_query='', request_body=''):
+		url = self.__sanitize_url(endpoint_url)
+		headers = self.__generate_headers(url, http_verb, request_query, request_body)
+		connection = http.client.HTTPSConnection(url.host)
+		connection.request(http_verb, url.to_text(), request_body, headers)
+
+		return connection.getresponse()
+
 
 	def perform_get(self, endpoint_url):
-		url = self.__sanitize_url(endpoint_url)
-		headers = self.__generate_headers(url, 'GET')
-		return requests.get(url.to_text(), headers)
+		return self.__api_call('GET', endpoint_url)
 
 
 	def perform_post(self, endpoint_url, request_query = '', request_body = ''):
---
+++ test_file.py
@@ -0,0 +1,9 @@
+import IssuetrakAPI
+
+key = IssuetrakAPI.read_api_key('api.key')['apiv1']
+url = 'https://harbert.issuetrak.com/api/v1'
+api = IssuetrakAPI.IssuetrakAPI(key, url)
+
+r = api.perform_get('users')
+print(r)
+print(r.status)
