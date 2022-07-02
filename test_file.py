a7ce4d4b131df356e3af8dfdf48f6d1fbf272b75
Will McElhenney <sir.sleepy93@gmail.com>
2022-07-02 01:28:26 -0500

    Removed test file

--- test_file.py
+++
@@ -1,9 +0,0 @@
-import IssuetrakAPI
-
-key = IssuetrakAPI.read_api_key('api.key')['apiv1']
-url = 'https://harbert.issuetrak.com/api/v1'
-api = IssuetrakAPI.IssuetrakAPI(key, url)
-
-r = api.perform_get('users')
-print(r)
-print(r.status_code)
