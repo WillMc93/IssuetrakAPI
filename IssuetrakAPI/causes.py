b2d74410d774d73fef2733e0ee2981ce7f20dd8f
Will McElhenney <whm0004@auburn.edu>
2022-07-12 17:03:41 -0500

    Began implementation

--- IssuetrakAPI/causes.py
+++ IssuetrakAPI/causes.py
@@ -1,4 +1,5 @@
 from IssuetrakAPI.api_connector import API_Connector
+from IssuetrakAPI.api_definitions import Connector_Cache
 
 
 class CausesMixIn(API_Connector):
@@ -7,11 +8,26 @@ class CausesMixIn(API_Connector):
 
 	def causes(self):
 		if self._causes is None:
-			return self._causes = Causes()
+			return self._causes = Causes(self.connector)
 		return self._causes
 
 
+class Causes(Connector_Cache):
+	def __clean_dict(json_data:dict) -> dict:
 
 
-class CausesMixIn(API_Connector):
-	
+	def get_by_id(cause_id:int) -> dict:
+		endpoint_url = f'causes/{cause_id}'
+		json_data = self.connector.perform_post(endpoint_url=endpoint_url)
+		data = __clean_dict(json_data)
+		return data
+
+	def get_all() -> dict:
+		endpoint_url = 'causes'
+		json_data = self.connector.perform_get(endpoint_url=endpoint_url)
+		data = __clean_dict(json_data)
+		return data
+
+
+
+
