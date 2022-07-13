1358ebd43f707156aa1afebc88f9518237c16cf1
Will McElhenney <whm0004@auburn.edu>
2022-07-13 15:32:37 -0500

    Implemented a generic class structure for generic calls to the API (just get by id and get all).

--- IssuetrakAPI/api_definitions.py
+++ IssuetrakAPI/api_definitions.py
@@ -11,4 +11,26 @@ class Connector_Cache:
 class RequestException(Exception):
 	def __init__(self, http_verb:str, url:str):
 		message = f'Request to API failed with verb: {http_verb} url: {url} combo.'
-		super().__init__(message)
+		super().__init__(message)class GenericCaller(ConnectorCache):
+	"""
+	A class for all requestors that just need a get by id, and a get all with
+	no server-side filtering/decoding.
+
+	Classes of this type include:
+		* Causes
+		* Classes
+		* Departments
+		* Locations
+		* Menu
+		* Organizations
+	"""
+	def __init__(self, connector:APIConnector, endpoint_url:str):
+		super(GenericCaller, self).__init__(connector)
+		self.endpoint_url = endpoint_url
+		
+	def get_by_id(_id:int) -> dict:
+		endpoint_url = f'{self.endpoint_url}/{_id}'
+		return self.connector.perform_post(endpoint_url=self.endpoint_url)
+
+	def get_all() -> dict:
+		return = self.connector.perform_get(endpoint_url=self.endpoint_url)
