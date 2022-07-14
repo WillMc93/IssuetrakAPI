76c8d926beeb0abf11dc948449a33895358da027
Will McElhenney <wmcelhenney93@gmail.com>
2022-07-13 21:55:32 -0500

    Made congruent

--- IssuetrakAPI/meta.py
+++ IssuetrakAPI/meta.py
@@ -1,10 +1,16 @@
 from IssuetrakAPI.api_connector import APIConnector
 from IssuetrakAPI.api_definitions import ConnectorCache
 
-class Meta(ConnectorCache):
-	def __init__(self, connector:APIConnector, endpoint_url:str):
-		super(Meta, self).__init__(connector)
-		self.endpoint_url = endpoint_url
+class MetaMixIn(APIConnector):
+	def metadata(self):
+		if self.metadata is None:
+			return self.metadata = Metadata(self, 'metadata/')
+		return self.metadata
 		
-	def get_all() -> dict:
+class Metadata(ConnectorCache):
+	def __init__(self, connector, endpoint_url):
+		self.connector = connector
+		self.endpoint_url = endpoint_url
+
+	def get() -> dict:
 		return = self.connector.perform_get(endpoint_url=self.endpoint_url)
