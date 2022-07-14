30f0b3ca5920e3e17f8334fb14e4d18849c2659e
Will McElhenney <wmcelhenney93@gmail.com>
2022-07-13 22:03:51 -0500

    Added doc strings describing class

--- IssuetrakAPI/user_defined_field_types.py
+++ IssuetrakAPI/user_defined_field_types.py
@@ -3,6 +3,9 @@ from IssuetrakAPI.api_definitions import GenericCaller
 
 
 class UserDefinedFieldTypesMixIn(APIConnector):
+	"""
+	Mix-in providing convenient namespace for User Defined Field Types
+	"""
 	def user_defined_field_types(self):
 		if self.user_defined_field_types is None:
 			return self.user_defined_field_types = UserDefinedFieldTypes(self, 'userdefinedfieldtypes/')
@@ -10,5 +13,9 @@ class UserDefinedFieldTypesMixIn(APIConnector):
 
 
 class UserDefinedFieldTypes(GenericCaller):
+	"""
+	A Generic Caller class that provides getting by ID and getting all for
+	User Defined Field Types
+	"""
 	def __init__(self, connector, endpoint_url):
 		super().__init__(connector, endpoint_url)
